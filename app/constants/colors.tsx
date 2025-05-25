4beeda64c7b029e38e3dcfe5142f524d953e0735
Gavishka <gavishka.rightmo@gmail.com>
2025-05-26 00:08:36 +0530

    add new color constants for black, gray, lightGray, and darkGray

--- app/constants/colors.tsx
+++ app/constants/colors.tsx
@@ -8,6 +8,10 @@ export const colors = {
     lightBlue: '#D3E3F1',
     imageBackground: '#F6F8FB',
     transparent: 'transparent',
+    black: '#000000',
+    gray: '#B0B0B0',
+    lightGray: '#F0F0F0',
+    darkGray: '#7D7D7D',
   };
   
   export const theme = {colors};
